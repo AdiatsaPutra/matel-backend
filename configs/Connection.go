ed8dbff3539a5cfd4a06575a0fddbd771e78a19e
Adiatsa Putra <santikosetyo77@gmail.com>
2023-05-13 09:42:18 +0700

    conn

--- configs/Connection.go
+++ configs/Connection.go
@@ -15,7 +15,7 @@ func InitDB() *gorm.DB {
 	val.Add("loc", "Asia/Jakarta")
 
 	// dsn := "root:1Ultramilk!@tcp(127.0.0.1:3306)/motor?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := "w08um7qaben07grspf9k:pscale_pw_VAkTxIR732WX6GQhmtlAamddhm7CSHSHhY69U2rjIm7@tcp(aws.connect.psdb.cloud)/matel?tls=true"
+	dsn := "w08um7qaben07grspf9k:pscale_pw_VAkTxIR732WX6GQhmtlAamddhm7CSHSHhY69U2rjIm7@tcp(aws.connect.psdb.cloud)/matel?tls=true&charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
