20510455757b5e9b32c3566d961448ae92904ddb
Adiatsa Putra <santikosetyo77@gmail.com>
2023-05-23 12:17:11 +0700

    sqlite

--- controllers/DownloadController.go
+++ controllers/DownloadController.go
@@ -10,12 +10,12 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func ExportHandler(c *gin.Context) {
-
 	// Retrieve all data from the table
 	var data []models.Leasing
 	err := config.InitDB().Find(&data).Error
@@ -23,6 +23,8 @@ func ExportHandler(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+
+	logrus.Info(data)
 
 	// Create a new SQLite database file
 	sqliteDB, err := gorm.Open(sqlite.Open("exported.db"), &gorm.Config{})
@@ -39,34 +41,46 @@ func ExportHandler(c *gin.Context) {
 		return
 	}
 
-	// Insert data into the SQLite database
-	sqliteDB.Create(&data)
-	// if err != nil {
-	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 	return
-	// }
+	batchSize := 100 // Set the desired batch size for insertion
+	totalData := len(data)
+	batchCount := totalData / batchSize
 
-	// Set the response headers for file download
-	filepath := "exported.db"
-	file, err := os.Open(filepath)
+	// Insert data into the SQLite database in batches
+	for i := 0; i <= batchCount; i++ {
+		start := i * batchSize
+		end := (i + 1) * batchSize
+		if end > totalData {
+			end = totalData
+		}
+
+		batch := data[start:end]
+		err = sqliteDB.Create(&batch).Error
+		if err != nil {
+			logrus.Info(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	// Create a zip archive
+	buf := new(bytes.Buffer)
+	zipWriter := zip.NewWriter(buf)
+
+	// Add the SQLite database file to the zip archive
+	filePath := "exported.db"
+	file, err := os.Open(filePath)
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 	defer file.Close()
 
-	// Get the file information.
 	fileInfo, err := file.Stat()
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 
-	// Create a zip archive.
-	buf := new(bytes.Buffer)
-	zipWriter := zip.NewWriter(buf)
-
-	// Create a new file in the zip archive.
 	header, err := zip.FileInfoHeader(fileInfo)
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
@@ -80,24 +94,23 @@ func ExportHandler(c *gin.Context) {
 		return
 	}
 
-	// Copy the file content to the zip archive.
 	_, err = io.Copy(writer, file)
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 
-	// Close the zip archive.
+	// Close the zip archive
 	err = zipWriter.Close()
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 
-	// Set the response headers.
+	// Set the response headers
 	c.Header("Content-Type", "application/zip")
 	c.Header("Content-Disposition", "attachment; filename=archive.zip")
 
-	// Send the zip file to the user.
+	// Send the zip file to the user
 	c.Data(http.StatusOK, "application/zip", buf.Bytes())
 }
