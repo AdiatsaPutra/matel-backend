f93eea7006d726b8085491046d160d5528c6aa02
Adiatsa Putra <santikosetyo77@gmail.com>
2023-05-24 18:22:43 +0700

    download db

--- main.go
+++ main.go
@@ -21,7 +21,6 @@ func main() {
 	r.GET("/leasing", controllers.GetLeasing)
 	r.POST("/upload-leasing", controllers.AddCSV)
 	r.GET("/export", controllers.ExportHandler)
-	r.GET("/export-test", controllers.ExportHandler)
 
 	r.GET("/province", controllers.GetProvince)
 	r.GET("/kabupaten/:province-id", controllers.GetKabupaten)
