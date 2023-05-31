1fe2e6f5b0095486d2d9c9588877056776bae29a
Adiatsa Putra <santikosetyo77@gmail.com>
2023-05-31 17:54:49 +0700

    download leasing

--- controllers/DownloadController.go
+++ controllers/DownloadController.go
@@ -3,88 +3,94 @@ package controllers
 import (
 	"archive/zip"
 	"io"
-	config "motor/configs"
-	"motor/models"
+
+	// config "motor/configs"
+	// "motor/models"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
+	// "gorm.io/driver/sqlite"
+	// "gorm.io/gorm"
 )
 
 func ExportHandler(c *gin.Context) {
-	// Retrieve all data from the table
-	var data []models.Leasing
-	err := config.InitDB().Select("nomorPolisi, noRangka, noMesin").Find(&data).Error
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+	// // Retrieve all data from the table
+	// var data []models.Leasing
+	// err := config.InitDB().Select("nomorPolisi, noRangka, noMesin").Limit(10000).Find(&data).Error
+	// if err != nil {
+	// 	logrus.Error(err)
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// 	return
+	// }
 
-	if len(data) == 0 {
-		c.JSON(http.StatusOK, gin.H{"message": "No data found"})
-		return
-	}
+	// if len(data) == 0 {
+	// 	c.JSON(http.StatusOK, gin.H{"message": "No data found"})
+	// 	return
+	// }
 
-	// Create a new SQLite database file
-	sqliteDB, err := gorm.Open(sqlite.Open("exported.db"), &gorm.Config{})
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	// defer sqliteDB.Close()
+	// // Create a new SQLite database file
+	// sqliteDB, err := gorm.Open(sqlite.Open("exported.db"), &gorm.Config{})
+	// if err != nil {
+	// 	logrus.Error(err)
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// 	return
+	// }
+	// // defer sqliteDB.Close()
 
-	// Clear existing data from the table
-	err = sqliteDB.Exec("DELETE FROM m_leasing").Error
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+	// // // Clear existing data from the table
+	// // err = sqliteDB.Exec("DELETE FROM m_leasing").Error
+	// // if err != nil {
+	// // 	logrus.Error(err)
+	// // 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// // 	return
+	// // }
 
-	// AutoMigrate your model in the SQLite database
-	err = sqliteDB.AutoMigrate(&models.Leasing{})
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+	// // AutoMigrate your model in the SQLite database
+	// err = sqliteDB.AutoMigrate(&models.LeasingToExport{})
+	// if err != nil {
+	// 	logrus.Error(err)
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// 	return
+	// }
 
-	batchSize := 10 // Set the desired batch size for insertion
-	totalData := len(data)
-	batchCount := totalData / batchSize
+	// batchSize := 10 // Set the desired batch size for insertion
+	// totalData := len(data)
+	// batchCount := totalData / batchSize
 
-	// Convert data to the desired struct with selected fields
-	var leasingData []models.Leasing
-	for _, d := range data {
-		logrus.Info(d)
-		leasingData = append(leasingData, models.Leasing{
-			NomorPolisi: d.NomorPolisi,
-			NoRangka:    d.NoRangka,
-			NoMesin:     d.NoMesin,
-		})
-	}
+	// // Convert data to the desired struct with selected fields
+	// var leasingData []models.LeasingToExport
+	// for _, d := range data {
+	// 	leasingData = append(leasingData, models.LeasingToExport{
+	// 		NomorPolisi: d.NomorPolisi,
+	// 		NoRangka:    d.NoRangka,
+	// 		NoMesin:     d.NoMesin,
+	// 	})
+	// }
 
-	// Insert data into the SQLite database in batches
-	for i := 0; i <= batchCount; i++ {
-		start := i * batchSize
-		end := (i + 1) * batchSize
-		if end > totalData {
-			end = totalData
-		}
+	// // Insert data into the SQLite database in batches
+	// for i := 0; i <= batchCount; i++ {
+	// 	start := i * batchSize
+	// 	end := (i + 1) * batchSize
+	// 	if end > totalData {
+	// 		end = totalData
+	// 	}
 
-		batch := leasingData[start:end]
-		err = sqliteDB.Create(&batch).Error
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-	}
+	// 	batch := leasingData[start:end]
+	// 	err = sqliteDB.Create(&batch).Error
+	// 	if err != nil {
+	// 		logrus.Error(err)
+	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// 		return
+	// 	}
+	// }
 
 	// Set the response headers for file download
 	filepath := "exported.db"
 	file, err := os.Open(filepath)
 	if err != nil {
+		logrus.Error(err)
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
@@ -149,5 +155,15 @@ func ExportHandler(c *gin.Context) {
 
 	// Remove file after sending it to the user.
 	os.Remove(zipFilePath)
+
+}
+
+func DownloadLeasing(c *gin.Context) {
+	zipFilePath := "archive.zip"
+
+	c.Writer.Header().Set("Content-Disposition", "attachment; filename="+zipFilePath)
+	c.Writer.Header().Set("Content-Type", "application/zip")
+
+	c.File(zipFilePath)
 
 }
--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 	r.GET("/leasing", controllers.GetLeasing)
 	r.POST("/upload-leasing", controllers.AddCSV)
 	r.GET("/export", controllers.ExportHandler)
+	r.GET("/download", controllers.DownloadLeasing)
 
 	r.GET("/province", controllers.GetProvince)
 	r.GET("/kabupaten/:province-id", controllers.GetKabupaten)
