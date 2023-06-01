5fbb7a4bc54d4081ddde56858f7d5a4cc8cc9198
reAlpha39 <yunusw718@gmail.com>
2023-06-01 10:24:09 +0700

    update dump sql

--- controllers/DumpDBController.go
+++ controllers/DumpDBController.go
@@ -26,27 +26,45 @@ func DumpSQLHandler(c *gin.Context) {
 	}
 	defer db.Close()
 
-	// Membuat file untuk menyimpan hasil dump
-	file, err := os.Create("exported.sql")
+	// Membuka file untuk menulis hasil dump
+	file, err := os.Create("dump.sql")
 	if err != nil {
-		log.Fatal("Failed to create exported SQL file:", err)
+		log.Fatalf("failed to create file: %v", err)
 	}
 	defer file.Close()
 
-	// Menginisialisasi string untuk menyimpan hasil dump
-	var dumpSQL string
-
-	// Mengambil hasil dump tabel menggunakan GORM
-	err = sourceDB.Model(&models.Leasing{}).Select("nomorPolisi, noMesin, noRangka").Find(&[]models.LeasingToExport{}).Error
+	// Menulis header SQL ke file
+	_, err = file.WriteString("INSERT INTO m_leasing (nomorPolisi, noMesin, noRangka) VALUES\n")
 	if err != nil {
-		log.Fatal("Failed to get dump table statement:", err)
+		log.Fatalf("failed to write to file: %v", err)
 	}
 
-	// Menyimpan hasil dump ke dalam file
-	_, err = file.WriteString(dumpSQL)
+	var leasings []models.LeasingToExport
+	err = sourceDB.Table("m_leasing").Select("nomorPolisi, noMesin, noRangka").Find(&leasings).Error
 	if err != nil {
-		log.Fatal("Failed to write dump to file:", err)
+		log.Fatalf("failed to fetch data from table: %v", err)
 	}
 
-	fmt.Println("Database dumped successfully")
+	// Menulis data ke file
+	for i, l := range leasings {
+		log.Printf("Writing data %d of %d\n", i+1, len(leasings))
+		_, err = file.WriteString(fmt.Sprintf("('%s', '%s', '%s')", l.NomorPolisi, l.NoMesin, l.NoRangka))
+		if err != nil {
+			log.Fatalf("failed to write to file: %v", err)
+		}
+
+		if i < len(leasings)-1 {
+			_, err = file.WriteString(",\n")
+			if err != nil {
+				log.Fatalf("failed to write to file: %v", err)
+			}
+		} else {
+			_, err = file.WriteString(";")
+			if err != nil {
+				log.Fatalf("failed to write to file: %v", err)
+			}
+		}
+	}
+
+	fmt.Println("Table dumped successfully!")
 }
