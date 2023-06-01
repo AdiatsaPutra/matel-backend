5cba31bd0c9e7cf091fe5303489b4213ed227698
reAlpha39 <yunusw718@gmail.com>
2023-06-01 10:33:34 +0700

    add archive to zip after dump to sql

--- controllers/DumpDBController.go
+++ controllers/DumpDBController.go
@@ -1,19 +1,17 @@
 package controllers
 
 import (
-	// "database/sql"
+	"archive/zip"
 	"fmt"
-	"log"
+	"io"
 	"os"
 
 	config "motor/configs"
 	"motor/models"
+	"motor/payloads"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	// "github.com/go-sql-driver/mysql"
-	// "gorm.io/driver/mysql"
-	// "gorm.io/gorm"
 )
 
 func DumpSQLHandler(c *gin.Context) {
@@ -27,44 +25,96 @@ func DumpSQLHandler(c *gin.Context) {
 	defer db.Close()
 
 	// Membuka file untuk menulis hasil dump
-	file, err := os.Create("dump.sql")
+	file, err := os.Create("export.sql")
 	if err != nil {
-		log.Fatalf("failed to create file: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"failed to create file: %v": err.Error()})
 	}
 	defer file.Close()
 
 	// Menulis header SQL ke file
 	_, err = file.WriteString("INSERT INTO m_leasing (nomorPolisi, noMesin, noRangka) VALUES\n")
 	if err != nil {
-		log.Fatalf("failed to write to file: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"failed to write header to file: %v": err.Error()})
 	}
 
 	var leasings []models.LeasingToExport
 	err = sourceDB.Table("m_leasing").Select("nomorPolisi, noMesin, noRangka").Find(&leasings).Error
 	if err != nil {
-		log.Fatalf("failed to fetch data from table: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"failed to fetch data from table: %v": err.Error()})
 	}
 
 	// Menulis data ke file
 	for i, l := range leasings {
-		log.Printf("Writing data %d of %d\n", i+1, len(leasings))
+		// log.Printf("Writing data %d of %d\n", i+1, len(leasings))
 		_, err = file.WriteString(fmt.Sprintf("('%s', '%s', '%s')", l.NomorPolisi, l.NoMesin, l.NoRangka))
 		if err != nil {
-			log.Fatalf("failed to write to file: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"failed to write to file: %v": err.Error()})
 		}
 
 		if i < len(leasings)-1 {
 			_, err = file.WriteString(",\n")
 			if err != nil {
-				log.Fatalf("failed to write to file: %v", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"failed to write to file: %v": err.Error()})
 			}
 		} else {
 			_, err = file.WriteString(";")
 			if err != nil {
-				log.Fatalf("failed to write to file: %v", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"failed to write to file: %v": err.Error()})
 			}
 		}
 	}
 
-	fmt.Println("Table dumped successfully!")
+	filepath := "export.db"
+
+	// Get the file information.
+	fileInfo, err := file.Stat()
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	// Create a zip archive.
+	zipFilePath := "archive.zip"
+	zipFile, err := os.Create(zipFilePath)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Failed to create ZIP file")
+		return
+	}
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	// Create a new file in the zip archive.
+	header, err := zip.FileInfoHeader(fileInfo)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	header.Name = fileInfo.Name()
+
+	writer, err := zipWriter.CreateHeader(&zip.FileHeader{
+		Name:   filepath,
+		Method: zip.Deflate, // Menggunakan metode kompresi Deflate
+	})
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	// Copy the file content to the zip archive.
+	_, err = io.Copy(writer, file)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	// Close the zip archive.
+	err = zipWriter.Close()
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	payloads.HandleSuccess(c, "Berhasil update database", "Berhasil", 200)
 }
