62587aed4a7a17af1809d8322df33833d011facc
Adiatsa Putra <santikosetyo77@gmail.com>
2023-06-09 10:49:15 +0700

    add limit

--- api/controllers/LeasingController.go
+++ api/controllers/LeasingController.go
@@ -5,7 +5,6 @@ import (
 	"matel/models"
 	"matel/payloads"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,22 +14,21 @@ func GetLeasing(c *gin.Context) {
 	limitStr := c.DefaultQuery("limit", "20") // Get the limit parameter from the query string
 	searchQuery := c.Query("search")          // Get the search query from the query string
 
-	page, err := strconv.Atoi(pageStr)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid page parameter"})
-		return
-	}
+	// page, err := strconv.Atoi(pageStr)
+	// if err != nil {
+	// 	c.JSON(400, gin.H{"error": "Invalid page parameter"})
+	// 	return
+	// }
 
-	limit, err := strconv.Atoi(limitStr)
-	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid limit parameter"})
-		return
-	}
+	// limit, err := strconv.Atoi(limitStr)
+	// if err != nil {
+	// 	c.JSON(400, gin.H{"error": "Invalid limit parameter"})
+	// 	return
+	// }
 
 	db := config.InitDB()
 
 	var leasing []models.Leasing
-	var total int64
 
 	query := db
 
@@ -38,19 +36,9 @@ func GetLeasing(c *gin.Context) {
 		query = query.Where("leasing LIKE ? OR cabang LIKE ? OR nomorPolisi LIKE ?", "%"+searchQuery+"%", "%"+searchQuery+"%", "%"+searchQuery+"%")
 	}
 
-	query.Find(&leasing)
-
-	if err := query.Model(&models.Leasing{}).Count(&total).Error; err != nil {
-		c.JSON(500, gin.H{"error": "Failed to retrieve leasing"})
-		return
-	}
-
-	offset := (page - 1) * limit
-	query = query.Offset(offset).Limit(1000)
-	query.Find(&leasing)
+	query.Find(&leasing).Limit(1000)
 
 	data := make(map[string]interface{})
-	data["total"] = total
 	data["leasing"] = leasing
 
 	payloads.HandleSuccess(c, data, "Leasing found", http.StatusOK)
