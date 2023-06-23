2480bc50d8acbe0d61c5ca84969f5e86b2d9a688
Adiatsa Putra <santikosetyo77@gmail.com>
2023-06-23 14:13:28 +0700

    gantikan data

--- api/controllers/DumpDBController.go
+++ api/controllers/DumpDBController.go
@@ -72,6 +72,12 @@ func DumpSQLHandler(c *gin.Context) {
 		}
 	}
 
+	_, err = file.WriteString("\n")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"failed to write delete query to file": err.Error()})
+		return
+	}
+
 	// Menulis header SQL ke file
 	_, err = file.WriteString("INSERT INTO m_kendaraan (id, cabang, nomorPolisi, noMesin, noRangka) VALUES\n")
 	if err != nil {
@@ -263,6 +269,12 @@ func UpdateSQLHandler(c *gin.Context) {
 				c.JSON(http.StatusInternalServerError, gin.H{"failed to write to file: %v": err.Error()})
 			}
 		}
+	}
+
+	_, err = file.WriteString("\n")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"failed to write delete query to file": err.Error()})
+		return
 	}
 
 	_, err = file.WriteString("DELETE FROM m_kendaraan\n")
