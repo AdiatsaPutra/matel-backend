2ea971e48e1c30d050015f66beb842ae2c53ab15
reAlpha39 <yunusw718@gmail.com>
2023-06-24 21:18:55 +0700

    update download path

--- api/controllers/DownloadController.go
+++ api/controllers/DownloadController.go
@@ -19,7 +19,7 @@ func DownloadLeasing(c *gin.Context) {
 
 
 func DownloadApk(c *gin.Context) {
-	fileApk := "./app/app-release.apk"
+	fileApk := "./release_app/app-release.apk"
 
 	c.Writer.Header().Set("Content-Disposition", "attachment; filename="+fileApk)
 	c.Writer.Header().Set("Content-Type", "application/vnd.android.package-archive")
