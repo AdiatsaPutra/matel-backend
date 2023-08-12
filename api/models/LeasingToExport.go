a999ad536b3968984df5aa19ad875cae6a48b77b
Adiatsa Putra <santikosetyo77@gmail.com>
2023-08-12 09:19:35 +0700

    cabang id str

--- api/models/LeasingToExport.go
+++ api/models/LeasingToExport.go
@@ -2,7 +2,7 @@ package models
 
 type LeasingToExport struct {
 	ID          string `gorm:"column:id"`
-	CabangID    int    `gorm:"column:cabang_id"`
+	CabangID    string `gorm:"column:cabang_id"`
 	Cabang      string `gorm:"column:cabang"`
 	NomorPolisi string `gorm:"column:nomorPolisi"`
 	NoRangka    string `gorm:"column:noRangka"`
