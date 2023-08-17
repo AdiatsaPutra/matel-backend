0a44f6dc2823929a68cb60585a2b30de29849ce4
Adiatsa Putra <santikosetyo77@gmail.com>
2023-08-17 12:59:06 +0700

    deleted at

--- api/controllers/DumpDBController.go
+++ api/controllers/DumpDBController.go
@@ -41,7 +41,6 @@ func DumpSQLHandler(c *gin.Context) {
 	var cabang []models.Cabang
 	err = sourceDB.Table("m_cabang").
 		Select("id, versi, versi_master").
-		Where("deleted_at IS NULL").
 		Find(&cabang).Error
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"failed to fetch data from table": err.Error()})
@@ -352,7 +351,7 @@ func UpdateSQLHandler(c *gin.Context) {
 	}
 
 	var dbData []MCabang
-	if err := config.InitDB().Find(&dbData).Error; err != nil {
+	if err := config.InitDB().Where("deleted_at IS NULL").Find(&dbData).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
