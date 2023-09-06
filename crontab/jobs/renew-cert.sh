dbdc9762baf08b60f81f387e2fd239a6bb685851
reAlpha39 <yunusw718@gmail.com>
2023-09-06 11:22:08 +0700

    update crontab

--- crontab/jobs/renew-cert.sh
+++ crontab/jobs/renew-cert.sh
@@ -1,2 +1,3 @@
-cd ~/matel-backend
-docker compose run --rm certbot renew && docker compose restart nginx
+cd /usr/scheduler
+# docker compose run --rm certbot renew && docker compose restart nginx
+docker compose ps
