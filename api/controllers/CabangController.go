7c438ca2e9a85ff16d784a04228277cb67b6445e
AdiatsaPutra <santikosetyo77@gmail.com>
2023-11-24 09:43:38 +0700

    cabang

--- api/controllers/CabangController.go
+++ api/controllers/CabangController.go
@@ -180,7 +180,7 @@ func exportToExcel(data []models.CabangExport, c *gin.Context) error {
 		cell = fmt.Sprintf("B%d", row+2)
 		file.SetCellValue(sheetName, cell, cabang.NoHP)
 		// Parse the string into time.Time
-		latestCreatedAt, err := time.Parse("2006-01-02T15:04:05.000Z", cabang.LatestCreatedAt)
+		latestCreatedAt, err := time.Parse(time.RFC3339, cabang.LatestCreatedAt)
 		if err != nil {
 			return err
 		}
