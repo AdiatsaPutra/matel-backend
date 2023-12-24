fbc59a215a0016f87fd24b0ba394271fb3ff3d50
reAlpha39 <yunusw718@gmail.com>
2023-12-24 07:45:21 +0700

    chore: bump app version

--- api/controllers/GetLatestVersionController.go
+++ api/controllers/GetLatestVersionController.go
@@ -23,7 +23,7 @@ func GetLatestVersion(c *gin.Context) {
 
 	data := make(map[string]interface{})
 
-	latestVersion := "1.9.0"
+	latestVersion := "1.10.0"
 
 	isMajorUpdate := strings.Split(body.Version, ".")[0] < strings.Split(latestVersion, ".")[0]
 	isMinorUpdate := strings.Split(body.Version, ".")[1] < strings.Split(latestVersion, ".")[1]
